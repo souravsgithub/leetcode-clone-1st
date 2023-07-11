d37d4bfd4f419650c4fe1af78da5a9c78e2699c3
Sourav <souravsemail2001@gmail.com>
2023-07-11 12:31:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require(express);
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from leetcode backend");
+});
+
+app.listen(port, () => {
+  console.log(`The express app is started on ${port}`);
+});
